feac42d1be28ac7b74621c1451ece4a19d8c7ed0
Baristopcu08 <112826011+Baristopcu08@users.noreply.github.com>
2023-04-27 00:05:06 +0300

    4 Commit

---
+++ src/test/java/Runner/LoginRunner.java
@@ -0,0 +1,17 @@
+package Runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+@CucumberOptions(
+        features = {"src/test/java/Feature/deneme.feature"},
+        glue = {"Steps"},
+        plugin = { "pretty",
+                "json:test-output/cucumber-reports/cucumber.json",
+                "html:test-output/cucumber-reports/cucumberreport.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
+public class LoginRunner  extends AbstractTestNGCucumberTests {
+
+
+
+}
